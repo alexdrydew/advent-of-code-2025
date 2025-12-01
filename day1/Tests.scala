dceb7da5a9263042be59fffecd1810c86ead3f9c
alexdrydew <alexdrydew@toloka.ai>
2025-12-01 21:31:07 +0100

    day1 part2

--- day1/Tests.scala
+++ day1/Tests.scala
@@ -1,12 +1,12 @@
-class MyTests extends munit.FunSuite:
+class CalculatePointingAtTest extends munit.FunSuite:
   test("Clockwise hits zero twice") {
     assertEquals(
       calculatePointingAtPosition(
         List(
-          ("R", 25),
-          ("R", 25),
-          ("R", 25),
-          ("R", 25)
+          ("R", 50),
+          ("R", 50),
+          ("R", 50),
+          ("R", 50)
         ),
         50,
         100,
@@ -23,7 +23,7 @@ class MyTests extends munit.FunSuite:
           ("R", 100),
           ("R", 100)
         ),
-        0,
+        50,
         100,
         0
       ),
@@ -35,12 +35,12 @@ class MyTests extends munit.FunSuite:
     assertEquals(
       calculatePointingAtPosition(
         List(
-          ("L", 25),
-          ("L", 25),
-          ("L", 25),
-          ("L", 25)
+          ("L", 50),
+          ("L", 50),
+          ("L", 50),
+          ("L", 50)
         ),
-        0,
+        50,
         100,
         0
       ),
@@ -79,5 +79,145 @@ class MyTests extends munit.FunSuite:
         0
       ),
       3
+    );
+  }
+
+class CalculateTimesPassedZeroTest extends munit.FunSuite:
+  test("Clockwise two full turns") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 100),
+          ("R", 100)
+        ),
+        50,
+        100
+      ),
+      2
+    );
+  }
+
+  test("Clockwise one and a half turns") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 150)
+        ),
+        50,
+        100
+      ),
+      2
+    );
+  }
+
+  test("Counterclockwise two full turns") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("L", 100),
+          ("L", 100)
+        ),
+        50,
+        100
+      ),
+      2
+    );
+  }
+
+  test("Counterclockwise one and a half turns") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("L", 150)
+        ),
+        50,
+        100
+      ),
+      2
+    );
+  }
+
+  test("Crossing zero multiple times") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 60), // 1, +10
+          ("L", 120), // 2, 90
+          ("R", 80), // 1, 70
+          ("L", 200) // 2, 70
+        ),
+        50,
+        100
+      ),
+      6
+    );
+  }
+
+  test("Starting at zero") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 50),
+          ("L", 150),
+          ("R", 250)
+        ),
+        0,
+        100
+      ),
+      4
+    );
+  }
+
+  test("No crossings") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 30),
+          ("L", 20),
+          ("R", 40)
+        ),
+        10,
+        100
+      ),
+      0
+    );
+  }
+
+  test("Exact hits at zero") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("R", 90), // 1, 0
+          ("L", 100), // 1, 0
+          ("R", 200), // 2, 0
+          ("R", 10), // 0, 10
+          ("L", 10) // 1, 0
+        ),
+        10,
+        100
+      ),
+      5
+    );
+  }
+
+  test("Complex sequence") {
+    assertEquals(
+      calculateTimesPassedZero(
+        List(
+          ("L", 68), // 1, 82
+          ("L", 30), // 0, 52,
+          ("R", 48), // 1, 0
+          ("L", 5), // 0, 95
+          ("R", 60), // 1, 55
+          ("L", 55), // 1, 0
+          ("L", 1), // 0, 99
+          ("L", 99), // 1, 0
+          ("R", 14), // 0, 14
+          ("L", 82) // 1, 32
+        ),
+        50,
+        100
+      ),
+      6
     );
   }
--- day1/main.scala
+++ day1/main.scala
@@ -23,6 +23,36 @@ def calculatePointingAtPosition(
   turns
 }
 
+def calculateTimesPassedZero(
+    rotations: IterableOnce[(Direction, Int)],
+    startingPosition: Int,
+    rotationPeriod: Int
+): Int = {
+  var position = startingPosition;
+  var turns = 0;
+
+  for ((direction, distance) <- rotations) {
+    val previousPosition = position;
+
+    position += (if (direction == "L") -distance else distance);
+    position = ((position % rotationPeriod) + rotationPeriod) % rotationPeriod;
+
+    // full turns
+    turns += distance / rotationPeriod
+
+    // check if partial turn passed zero
+    direction match {
+      case "L" =>
+        if (position > previousPosition || position == 0) && previousPosition != 0
+        then turns += 1
+      case "R" =>
+        if position < previousPosition then turns += 1
+    }
+  }
+
+  turns
+}
+
 val parseLine: String => Option[(Direction, Int)] = {
   case s"L$d" => Some("L", d.toInt)
   case s"R$d" => Some("R", d.toInt)
@@ -30,15 +60,29 @@ val parseLine: String => Option[(Direction, Int)] = {
 }
 
 @main
-def solve(): Unit = {
+def solve(part: String): Unit = {
   val rotations =
-    Source.fromFile("day1/input.txt").getLines().map(parseLine).map(_.get)
-  val position = calculatePointingAtPosition(
-    rotations,
-    startingPosition = 50,
-    rotationPeriod = 100,
-    targetPosition = 0
-  )
+    Source.fromFile("day1/input.txt").getLines().map(parseLine).map(_.get);
 
-  println(position);
+  part match {
+    case "part1" =>
+      val times = calculatePointingAtPosition(
+        rotations,
+        startingPosition = 50,
+        rotationPeriod = 100,
+        targetPosition = 0
+      )
+
+      println(times);
+    case "part2" =>
+      val position = calculateTimesPassedZero(
+        rotations,
+        startingPosition = 50,
+        rotationPeriod = 100
+      )
+
+      println(position);
+    case _ =>
+      println("Unknown part")
+  };
 }
