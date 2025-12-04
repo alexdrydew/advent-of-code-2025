20ce91568feefdfacfe82c639157d227d27f7061
alexdrydew <alexdrydew@toloka.ai>
2025-12-04 23:06:42 +0100

    day2 part2

--- day2/Tests.scala
+++ day2/Tests.scala
@@ -1,7 +1,7 @@
 class GetRepeatedNumsSumTest extends munit.FunSuite:
   test("test 1") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         1188511880,
         1188511890
       ),
@@ -11,7 +11,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test 2") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         12001200,
         12011201
       ),
@@ -21,7 +21,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test 3") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         95,
         115
       ),
@@ -31,7 +31,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test 4") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         11,
         22
       ),
@@ -41,7 +41,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test 5") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         797927,
         798999
       ),
@@ -51,7 +51,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test 6") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         307,
         1038
       ),
@@ -61,7 +61,7 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test odd") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         422394377,
         422468141
       ),
@@ -71,10 +71,105 @@ class GetRepeatedNumsSumTest extends munit.FunSuite:
 
   test("test odd 2") {
     assertEquals(
-      getRepeatedNumsSum(
+      getRepeatedTwiceInRangeSum(
         100,
         1010
       ),
       1010L
+    );
+  }
+
+class GetRepeatedInRangeSumTest extends munit.FunSuite:
+  test("test 1") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        998,
+        1012
+      ),
+      999 + 1010L
+    );
+  }
+
+  test("test 2") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        1200,
+        2222
+      ),
+      1212L + 1313L + 1414L + 1515L + 1616L + 1717L + 1818L + 1919L + 2020L + 2121L + 2222L
+    );
+  }
+
+  test("test 3") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        1188511880,
+        1188511890
+      ),
+      1188511885L
+    );
+  }
+
+  test("test 4") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        115,
+        282
+      ),
+      222L
+    );
+  }
+
+  test("test 5") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        422394377,
+        422468141
+      ),
+      422422422L
+    );
+  }
+
+  test("test 6") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        2244,
+        5558
+      ),
+      3333 + 4444 + 5555L
+        + 2323 + 2424 + 2525 + 2626 + 2727 + 2828 + 2929 + 3030 + 3131
+        + 3232 + 3434 + 3535 + 3636 + 3737 + 3838 + 3939 + 4040 + 4141
+        + 4242 + 4343 + 4545 + 4646 + 4747 + 4848 + 4949 + 5050 + 5151
+        + 5252 + 5353 + 5454
+    );
+  }
+
+  test("test 7") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        5,
+        100
+      ),
+      11L + 22 + 33 + 44 + 55 + 66 + 77 + 88 + 99L
+    );
+  }
+
+  test("test 8") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        0,
+        9
+      ),
+      0L
+    );
+  }
+
+  test("test 9") {
+    assertEquals(
+      getRepeatedInRangeSum(
+        10000000,
+        12121212L
+      ),
+      (1000 to 1212).map(i => i.toString.repeat(2).toLong).sum
     );
   }
--- day2/main.scala
+++ day2/main.scala
@@ -1,6 +1,7 @@
 import scala.io.Source
+import scala.collection.mutable.HashMap
 
-def getRepeatedNumsSum(min: Long, max: Long): Long = {
+def getRepeatedTwiceInRangeSum(min: Long, max: Long): Long = {
   val minStr = min.toString;
   val length = minStr.length;
 
@@ -21,12 +22,83 @@ def getRepeatedNumsSum(min: Long, max: Long): Long = {
   sum
 }
 
+def divCeil[T](a: T, b: T)(using num: Integral[T]): T =
+  num.quot(num.plus(a, num.minus(b, num.one)), b)
+
+def factorize(n: Long): Set[Int] = {
+  var factors = Set[Int]();
+
+  for i <- 1L to Math.sqrt(n).toLong do
+    if n % i == 0 then {
+      factors += i.toInt;
+      factors += (n / i).toInt;
+    }
+
+  factors
+}
+
+def getRepeatedInRangeSum(min: Long, max: Long): Long = {
+  val maxStr = max.toString;
+  val minStr = min.toString;
+
+  var sum = 0L;
+  for numLength <- minStr.length to maxStr.length do
+    var partLengths =
+      factorize(numLength).filter(_ < numLength).toList.sorted.reverse
+
+    def getRepeatedSum(repeatedPartLength: Int): Long = {
+      var current = "1".padTo(repeatedPartLength, '0').toLong;
+      val repeats = numLength / repeatedPartLength;
+
+      var sum = 0L;
+
+      var repeated = 0L;
+      val currentMax = Math.min(
+        "9".repeat(repeatedPartLength * repeats).toLong,
+        max
+      )
+
+      while ({
+        repeated = current.toString.repeat(repeats).toLong;
+        repeated <= currentMax;
+      }) {
+        if repeated >= min then sum += repeated;
+        current += 1;
+      }
+
+      sum
+    };
+
+    val partLengthToSum = HashMap.from(
+      partLengths.map(i => i -> getRepeatedSum(i))
+    )
+
+    for factor <- partLengths.reverse do
+      for multiple <- partLengths do
+        if multiple % factor == 0 && multiple != factor then
+          partLengthToSum(multiple) -= partLengthToSum(factor);
+
+    sum += partLengthToSum.values.sum;
+  sum
+}
+
 @main
-def main(): Unit = {
+def main(part: String): Unit = {
   val ranges = Source.fromFile("day2/input.txt").mkString.strip;
   val parts = ranges.split(",").map { r => r.split("-") };
-  var sum = parts.map { case Array(min, max) =>
-    getRepeatedNumsSum(min.toLong, max.toLong)
-  }.sum;
-  println(sum);
+
+  part match {
+    case "part1" => {
+      var sum = parts.map { case Array(min, max) =>
+        getRepeatedTwiceInRangeSum(min.toLong, max.toLong)
+      }.sum;
+      println(sum);
+    }
+    case "part2" => {
+      var sum = parts.map { case Array(min, max) =>
+        getRepeatedInRangeSum(min.toLong, max.toLong)
+      }.sum;
+      println(sum);
+    }
+  }
 }
